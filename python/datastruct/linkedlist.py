7a4c4fc49b158d297b473640f29da798ea139a60
unknown <alberto.re@gmail.com>
2019-01-27 21:00:39 +0100

    Enriched linkedlist interface and added docstrings

--- python/datastruct/linkedlist.py
+++ python/datastruct/linkedlist.py
@@ -2,6 +2,8 @@ from typing import Any, List, Optional
 
 
 class Node:
+    """A node object. It consists of a value and a reference
+    to the next element in the list."""
 
     def __init__(self, value: Any, nxt: 'Node' = None) -> None:
         self._value = value
@@ -24,6 +26,7 @@ class Node:
 
 
 class LinkedList:
+    """A singly-linked linked list data structure."""
 
     def __init__(self, array: List[Any] = None) -> None:
         self._head: Optional[Node] = None
@@ -34,10 +37,21 @@ class LinkedList:
         self._cur_node: Optional[Node] = None
 
     def is_empty(self) -> bool:
+        """Is the list empty?"""
         return self._head is None
 
     def append(self, value: Any) -> None:
+        """Adds an element to the end of the list."""
         self._append(value)
+
+    def prepend(self, value: Any) -> None:
+        """Adds an element to the beginning of the list."""
+        if self._head is not None:
+            second = self._head
+            self._head = Node(value, second)
+        else:
+            self._head = Node(value)
+        self._len += 1
 
     def _append(self, value: Any) -> None:
         if self._head is not None:
@@ -59,10 +73,10 @@ class LinkedList:
     def __next__(self) -> Any:
         if self._cur_node:
             self._cur_node = self._cur_node.next
-            if self._cur_node is None:
-                raise StopIteration
         else:
             self._cur_node = self._head
+        if self._cur_node is None:
+            raise StopIteration
         return self._cur_node.value
 
     def __str__(self) -> str:
@@ -73,4 +87,4 @@ class LinkedList:
             while cur.next is not None:
                 cur = cur.next
                 values.append(cur.value)
-        return "[" + ",".join(map(str, values)) + "]"
+        return "[" + ", ".join(map(str, values)) + "]"
--- python/test/datastruct/test_linkedlist.py
+++ python/test/datastruct/test_linkedlist.py
@@ -7,27 +7,32 @@ class LinkedListTest(unittest.TestCase):
 
     def test_init_empty(self):
         ll = LinkedList()
-        self.assertEqual(0, len(ll))
+        self.assertEqual([], list(ll))
 
     def test_init_from_array(self):
         lst = [1, 2, 3, 4, 5]
         ll = LinkedList(lst)
-        print(ll)
-        self.assertEqual(len(lst), len(ll))
-        for index, element in enumerate(ll):
-            self.assertEqual(lst[index], element)
+        self.assertEqual(lst, list(ll))
 
     def test_append(self):
         ll = LinkedList()
-        self.assertEqual(0, len(ll))
         ll.append(1)
-        self.assertEqual(1, len(ll))
+        ll.append(2)
+        ll.append(3)
+        self.assertEqual([1, 2, 3], list(ll))
+
+    def test_prepend(self):
+        ll = LinkedList()
+        ll.prepend(1)
+        ll.prepend(2)
+        ll.prepend(3)
+        self.assertEqual([3, 2, 1], list(ll))
 
     def test_str(self):
         ll = LinkedList()
         self.assertEqual("[]", str(ll))
         ll = LinkedList([1, 2, "3", 4, False])
-        self.assertEqual("[1,2,3,4,False]", str(ll))
+        self.assertEqual("[1, 2, 3, 4, False]", str(ll))
 
 
 if __name__ == '__main__':
